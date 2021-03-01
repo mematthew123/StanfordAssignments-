4f312af5f93ef56325b2e9798b7e43169a7741b7
matthew <mematthew123@gmail.com>
2021-02-28 21:47:07 -0700

    week 3 assignments

---
+++ Stanford106AAssignments/src/week6/Main.java
@@ -0,0 +1,20 @@
+package week6;
+
+import java.util.Scanner;
+
+public class Main {
+	  public static void main(String[] args) {
+	    System.out.println("Good Job!!");
+	
+	  
+	  }
+	
+
+}
+
+
+
+	
+
+
+
