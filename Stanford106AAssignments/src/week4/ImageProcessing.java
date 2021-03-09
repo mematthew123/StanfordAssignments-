b995cd67783b5c255a3dc54d8809f6df8d053241
matthew <mematthew123@gmail.com>
2021-03-08 18:02:22 -0700

    week 4

--- Stanford106AAssignments/src/week4/ImageProcessing.java
+++ Stanford106AAssignments/src/week4/ImageProcessing.java
@@ -1,7 +1,33 @@
 package week4;
 
-import acm.program.GraphicsProgram;
-
+import acm.graphics.*;
+import acm.program.*;
 public class ImageProcessing extends GraphicsProgram {
-
+   public void run() {
+      GImage original = new GImage("Candle.gif");
+      GImage rotated = rotateLeft(original);
+      double x0 = getWidth() / 2 - original.getWidth() - 5;
+      double y0 = (getHeight() - original.getHeight()) / 2;
+      double x1 = getWidth() / 2 + 5;
+      double y1 = (getHeight() - rotated.getHeight()) / 2;
+      add(original, x0, y0);
+      add(rotated, x1, y1);
+}
+/**
+ * Creates a new image which consists of the bits in the original
+ * rotated left by 90 degrees.
+ * @param image The original image
+ * @return The image rotated left by 90 degrees
+ */
+   private GImage rotateLeft(GImage image) {
+      int[][] oldPixels = image.getPixelArray();
+      int width = oldPixels[0].length;
+      int height = oldPixels.length;
+      int[][] newPixels = new int[width][height];
+      for (int i = 0; i < height; i++) {
+         for (int j = 0; j < width; j++) {
+            newPixels[width - j - 1][i] = oldPixels[i][j];
+} }
+      return new GImage(newPixels);
+   }
 }
--- Stanford106AAssignments/src/week4/WordCount.java
+++ Stanford106AAssignments/src/week4/WordCount.java
@@ -2,10 +2,72 @@ package week4;
 
 import acm.program.ConsoleProgram;
 
-public class WordCount extends ConsoleProgram {
+import java.io.*;
+import java.nio.file.FileSystem;
+import java.util.*;
+import java.util.StringTokenizer;
+import javax.naming.ldap.Rdn;
+
+public class WordCount<start> extends ConsoleProgram {
+
 	
+
 	public void run() {
+		
+		String strCurrentLine = null;
+		int wordsCount = 0;
+		int charCount = 0;
+		int linesCount = 0;
+		try (BufferedReader br = new BufferedReader(new FileReader("/Users/matthewrhoads/Desktop/Merit America/TestFile.txt"))) {
+			while ((strCurrentLine = br.readLine()) != null) {
+							
+				
+				String line = strCurrentLine;
+				
+	            if (line == null) break;
+	            println (line);
+	            linesCount++;
+	            wordsCount += countWords(line);
+	            charCount += line.length();
+	           // linesCount += countLines();
+			}
+			
+			
+				
+				
+			
+				
+				println("Lines = " + linesCount);
+				println("Words = " + wordsCount);
+			    println("Chars = " + charCount);
+
+
+				
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace(); 
+			}
+	}
+
+
+
+
+	private int countWords(String line) {
+		StringTokenizer st = new StringTokenizer (line," ");
+		int wordCount = 0;
+		while (st.hasMoreTokens()){
+			String word = st.nextToken();
+			if (!word.equals("  "))
+				wordCount++;
+		} 
+		return wordCount;
+		
+		
+
+		
 
 	}
-	
 }
